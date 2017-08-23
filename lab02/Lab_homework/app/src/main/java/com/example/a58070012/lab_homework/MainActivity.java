0c9574f6f0be26861c29bbe83987042e7f41db47
58070012 <58070012@kmitl.ac.th>
2017-08-23 23:11:41 +0700

    send homework no button implimentation yet

---
+++ lab02/Lab_homework/app/src/main/java/com/example/a58070012/lab_homework/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.a58070012.lab_homework;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.homework_01);
+    }
+}
