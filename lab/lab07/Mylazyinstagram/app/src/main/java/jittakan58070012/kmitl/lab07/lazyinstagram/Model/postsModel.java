392c5c0e2ce0345dbfb65efb4c55471ad6c3e961
aliveable <aliveable1754@gmail.com>
2017-10-18 01:15:05 +0700

    updated patch

---
+++ lab/lab07/Mylazyinstagram/app/src/main/java/jittakan58070012/kmitl/lab07/lazyinstagram/Model/postsModel.java
@@ -0,0 +1,36 @@
+package jittakan58070012.kmitl.lab07.lazyinstagram.Model;
+
+/**
+ * Created by SKpro on 10/11/2017 AD.
+ */
+
+public class postsModel {
+
+    private int comment;
+    private int like;
+    private String url;
+
+    public int getComment() {
+        return comment;
+    }
+
+    public void setComment(int comment) {
+        this.comment = comment;
+    }
+
+    public int getLike() {
+        return like;
+    }
+
+    public void setLike(int like) {
+        this.like = like;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
